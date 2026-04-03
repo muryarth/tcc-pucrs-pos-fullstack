1685ec4826e1f41773fc88c6e146b184c74a93bf
Arthur Mury <arthur.mury@outlook.com>
2026-04-03 20:47:35 -0300

    feat: subindo versão inicial de serviço de prontuários em java

---
+++ app-back-odonto-manager/api-records/src/test/java/com/muryarth/odonto/api_records/ApiRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muryarth.odonto.api_records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
